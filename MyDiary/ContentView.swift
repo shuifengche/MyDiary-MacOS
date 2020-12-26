42eb0de1608dda3bea77f5104d901d293b25a8a2
shuifengche <shuifengche@foxmail.com>
2020-12-27 00:55:56 +0800

    Xcode demo

---
+++ MyDiary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyDiary
+//
+//  Created by 水风车 on 2020/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyDiary/MyDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDiaryApp.swift
+//  MyDiary
+//
+//  Created by 水风车 on 2020/12/27.
+//
+
+import SwiftUI
+
+@main
+struct MyDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
